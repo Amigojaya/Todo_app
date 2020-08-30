74d798d4dec238373ab3aa54a2a1a1181c670741
jayamouli <moulijaya@gmail.com>
2020-08-30 14:54:44 +0530

    added javascripts for auto updating

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,5 @@
 class Todo < ApplicationRecord
   belongs_to :user_task
+  #because cocoon gem retrive by created_at but its not best idea so i added this 
+  default_scope { order('id ASC') } 
 end
