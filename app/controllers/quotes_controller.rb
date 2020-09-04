fe9a5a842c628072aeba8554cc74e30031672194
jayamouli <moulijaya@gmail.com>
2020-09-04 20:36:41 +0530

    added user and admin quotes pages

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,19 @@
 class QuotesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @quotes = Quote.all.where(status: "approved")
-    @random = Quote.order("RANDOM()").where(status: "approved").limit(10)
+    @quotes = Quote.all.where(status: "approved").order(:id)
+    @random = Quote.order("RANDOM()").where(status: "approved").limit(6)
   end
 
   def new
     @quote = Quote.new
+  end
+
+  def user
+    @random = Quote.order("RANDOM()").where(status: "approved").limit(6)
+    @pending = current_user.quotes.order(:id)
+    @count_approve = Quote.all.where(status: "approved").count.to_s
+    @filter = "Click to view all " + @count_approve + " Quotes"
   end
 
   def create
@@ -26,6 +33,15 @@ class QuotesController < ApplicationController
     end
   end
 
+  def admin
+    if current_user.admin
+      @quotes = Quote.all.order(:id)
+      @pending = Quote.where(status: "pending").order(:id)
+    else
+      redirect_to quotes_path
+    end
+  end
+
   def update
     @quote = Quote.find(params[:id])
     if @quote.update(quote_params_good)
@@ -38,12 +54,16 @@ class QuotesController < ApplicationController
   def destroy
     @quote = Quote.find(params[:id])
     @quote.destroy
-    redirect_to quotes_path
+    if current_user.admin
+      redirect_to admin_quote_path
+      else
+      redirect_to quotes_path
+    end
   end
 
   private 
 
   def quote_params_good
-    params.require(:quote).permit(:quotes, :author, :user_id)
+    params.require(:quote).permit(:quotes, :author, :user_id, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   get 'manage', to: 'user_tasks#manage', as: 'manage'
 
+  get 'admin/quotes', to: 'quotes#admin', as: 'admin_quote'
+
+  get 'user/quotes', to: 'quotes#user', as: 'user_quote'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   match 'download', to: 'home#download_pdf', as: 'download', via: :get
